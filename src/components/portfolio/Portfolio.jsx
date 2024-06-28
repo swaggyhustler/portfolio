0bdf22dea9eeeaf75d9d85ae343f786201abdd50
swaggyhustler <gedamsubhash086@gmail.com>
2024-06-28 23:55:06 +0530

    Add description to projects section (portfolio)

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,25 +8,37 @@ const items=[
         id: 1,
         img: "https://images.pexels.com/photos/386009/pexels-photo-386009.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         title: "Travel Tracker",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa ea ipsam aspernatur voluptatibus, earum ipsa impedit suscipit eaque provident cupiditate id animi veritatis, harum illum esse odit unde pariatur repudiandae?"
+        desc: `This application allows multiple users to locate countries that they have visited, highlight them according to
+         the input of the user. It leverages the extensive use of Database and EJS to dynamically fetch and display the output
+          to the user. Multiple users are able to log their details and access the data related to the each user on the single 
+          click of a button on the Website. The main challenge that I've faced during building this project involved in managing 
+          the database for each user.`
     },
     {
         id: 2,
         img: "https://images.pexels.com/photos/936611/pexels-photo-936611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         title: "Healthy Diet",
-        desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odio in sit nam iste doloribus inventore quod? Optio beatae commodi repellendus vero, suscipit ex ratione reprehenderit quibusdam animi, tempora in ducimus."
+        desc: `This application prompt users for their details regarding health parameters and personal interests of food and generates food
+              suggestions for the input. It leverages the use of Python Framework for Web Development called Flask which is considered to be the
+              light weighted Framework for Web Development, and also the usage of API's (Open AI API) for the dynamic suggestions.
+              It also consist of CSS Framework called Bootstrap, for the ease of styling and layout of the website. The main challenge that I have
+              faced during development of this project was mainly integration of the Public API to the website.`
     },
     {
         id: 3,
         img: "https://images.pexels.com/photos/733856/pexels-photo-733856.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         title: "Blogging Application",
-        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores alias dolore ullam aut sint sequi natus illo neque quas architecto tempora veniam laboriosam, vitae dolor minus! Corporis eveniet quo repudiandae!"
+        desc: `This is application that enables users to create the blog of their choice, prompting the Title, Author and the Content related to the blog
+              to be posted. User will be able to list down the number of blogs the has been posted in the Explore section of the application. It
+              demonstrates the extensive use of EJS (Embedded Javascript) and it's usage in building Dynamic Web Applications.`
     },
     {
         id: 4,
         img: "https://images.pexels.com/photos/5849577/pexels-photo-5849577.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         title: "Wallet Watch",
-        desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat repellendus sapiente error dolorum atque voluptatum veniam nihil illo animi. Ut, iure. Eaque recusandae cum modi, minima magnam at optio dignissimos."
+        desc: `This is an application where the user will be able to track their expenses and log their all the information about the monetary
+              transactions. It uses the MongoDB Atlas (Cloud Storage) to persist the data and ensuring the availability upto 99.99%. It also
+              leverages the extensive use of React Framework for building frontend of the application.`
     }
 ];
 
